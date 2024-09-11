30e6ac960597348fffca44dc46834f657f6b272c
Thiago Goncalves <thiagosg80@gmail.com>
2024-09-11 15:47:58 -0300

    Feito o layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatButtonModule
+  ],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'lm-frontend';
+  clientInput: string = '';
 }
